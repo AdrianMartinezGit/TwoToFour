16773b12c315b74ac96fcc272bfd3b0c06c6ddc3
AdrianMartinezGit <adrionmtz02@gmail.com>
2024-01-09 15:17:04 -0800

    Remove 'Using' Keyword

--- Services/AskQuestion/AskQuestionService.cs
+++ Services/AskQuestion/AskQuestionService.cs
@@ -1,9 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.Extensions.ObjectPool;
-
 namespace TwoToFour.Services.AskQuestion;
 
 public class AskQuestionService : IAskQuestionService
--- Services/AskQuestion/IAskQuestionService.cs
+++ Services/AskQuestion/IAskQuestionService.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace TwoToFour.Services.AskQuestion;
 
 public interface IAskQuestionService {
